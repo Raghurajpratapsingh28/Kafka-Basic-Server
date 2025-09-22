771c28e9c8b8e762fa424492f5aa90766d4f6fea
Raghuraj Pratap Singh <Raghurajpratap2810@gmail.com>
2025-09-23 00:31:31 +0530

    run like a cli

--- producer.js
+++ producer.js
@@ -1,25 +1,36 @@
-const {kafka} = require('./client');
+const { kafka } = require("./client");
+const readline = require("readline");
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+async function init() {
+  const producer = kafka.producer();
 
-async function init(){
-    const producer = kafka.producer();
-    console.log("connecting producer..........")
-    await producer.connect();
-    console.log("producer connected........")
-     
+  console.log("Connecting Producer");
+  await producer.connect();
+  console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
     await producer.send({
-        topic:"rider-updates",
-        messages:[{value:JSON.stringify({
-            riderId:1,
-            riderName:"John Doe",
-            riderLocation:"New York",
-            riderStatus:"active"
-        })}]    
-    })
-    console.log("message sent........")
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
     await producer.disconnect();
-    console.log("producer disconnected........")
+  });
 }
 
 init();
